cdca461455ceead5273a6b4dffd62d910bfbbc5f
zhaixiaobin <zhhai-xiaobin@hotmail.com>
2019-04-27 17:22:07 +0800

    1、变更文件目录； 2、chrome自动化抓取测试。

---
+++ gather/src/test/java/com/skyworthbox/chrome/auto/DocGather.java
@@ -0,0 +1,94 @@
+package com.skyworthbox.chrome.auto;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+
+public class DocGather {
+    private static final String CHROME_EXE = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe";
+    private static final String DRIVER_PATH = "./src/main/driver/chromedriver_win32/chromedriver_74.0.3729.6.exe";
+
+    static {
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+    }	
+	
+	private Selenium selenium;
+
+	public static ChromeDriver getDriver() {
+		// String userAgent = AppForum.getAgents();
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, buildOption());
+		capabilities.setJavascriptEnabled(true);
+		ChromeDriver driver = new ChromeDriver(capabilities);
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.setLogLevel(Level.ALL);
+
+		return driver;
+	}
+	
+    static ChromeOptions buildOption() {
+        String downloadFilepath = "f:\\Users";
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary(CHROME_EXE);
+        // to disable browser extension popup
+        options.addArguments("--no-sandbox", "--test-type", "--headless", "--disable-gpu", "--disable-extensions");
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+        
+        return options;
+    }	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		WebDriver driver = getDriver();
+		String baseUrl = "https://www.katalon.com/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
+	}
+
+	@Test
+	public void testGather() throws Exception {
+		selenium.open("http://wenshu.court.gov.cn/list/list/?sorttype=1&number=0.5191832203422626&guid=8a744b71-75c1-e1158bfd-5c764e544ebd&conditions=searchWord+2+AJLX++%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E6%B0%91%E4%BA%8B%E6%A1%88%E4%BB%B6&conditions=searchWord+%E6%B0%91%E4%BA%8B%E6%A1%88%E7%94%B1+++%E4%B8%80%E7%BA%A7%E6%A1%88%E7%94%B1:%E6%B0%91%E4%BA%8B%E6%A1%88%E7%94%B1");
+		selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[1]/following::img[2]");
+		selenium.selectWindow("win_ser_1");
+		selenium.close();
+		selenium.selectWindow("win_ser_local");
+		selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[2]/following::img[2]");
+		selenium.selectWindow("win_ser_2");
+		selenium.close();
+		selenium.selectWindow("win_ser_local");
+		selenium.click("link=2");
+		selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='（2014）格执字第101-3号'])[1]/following::img[2]");
+		selenium.selectWindow("win_ser_3");
+		selenium.close();
+		selenium.selectWindow("win_ser_local");
+		selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[2]/following::img[2]");
+		selenium.selectWindow("win_ser_4");
+		selenium.close();
+		selenium.selectWindow("win_ser_local");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
---
+++ gather/src/test/java/com/skyworthbox/firefox/FirefoxGatherTest.java
@@ -0,0 +1,84 @@
+package com.skyworthbox.firefox;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+
+public class FirefoxGatherTest {
+    private Selenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println(LocalDateTime.now().toString());
+        String basePath = "./src/main/driver/";
+        String downloadDir = "./src/main/download";
+
+        File file = new File(basePath);
+        System.out.println(file.getAbsolutePath() + "," + file.exists());
+
+        String firefoxDir = basePath + "/Mozilla Firefox/firefox.exe";
+        if(!new File(firefoxDir).exists()) {
+            System.out.println("use system default firefox app.");
+            firefoxDir = "D:/Program Files/Mozilla Firefox/firefox.exe";
+        }
+        
+        System.setProperty("webdriver.firefox.bin", firefoxDir);
+        System.setProperty("webdriver.gecko.driver", basePath + "geckodriver-v0.24.0-win64/geckodriver.exe");
+
+        FirefoxOptions options = new FirefoxOptions();
+        options.setLogLevel(FirefoxDriverLogLevel.DEBUG);
+        options.setHeadless(true);
+        WebDriver driver = new FirefoxDriver(options);
+
+        FirefoxProfile profile = new FirefoxProfile();
+        profile.setPreference("browser.download.folderList", 2);
+        profile.setPreference("browser.download.dir", downloadDir);
+        profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "binary/octet-stream");
+
+        String baseUrl = "https://www.katalon.com/";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+        System.out.println(LocalDateTime.now().toString());
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        selenium.open(
+                "http://wenshu.court.gov.cn/list/list/?sorttype=1&number=0.5191832203422626&guid=8a744b71-75c1-e1158bfd-5c764e544ebd&conditions=searchWord+2+AJLX++%E6%A1%88%E4%BB%B6%E7%B1%BB%E5%9E%8B:%E6%B0%91%E4%BA%8B%E6%A1%88%E4%BB%B6&conditions=searchWord+%E6%B0%91%E4%BA%8B%E6%A1%88%E7%94%B1+++%E4%B8%80%E7%BA%A7%E6%A1%88%E7%94%B1:%E6%B0%91%E4%BA%8B%E6%A1%88%E7%94%B1");
+
+        System.out.println(LocalDateTime.now().toString());
+        selenium.waitForPageToLoad("5000");
+        System.out.println(LocalDateTime.now().toString());
+        selenium.click("id=condtion");
+        selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[1]/following::img[2]");
+        selenium.selectWindow("win_ser_1");
+        selenium.close();
+        selenium.selectWindow("win_ser_local");
+        selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[2]/following::img[2]");
+        selenium.selectWindow("win_ser_2");
+        selenium.close();
+        selenium.selectWindow("win_ser_local");
+        selenium.click("xpath=(.//*[normalize-space(text()) and normalize-space(.)='【裁判理由】'])[4]/following::img[2]");
+        selenium.selectWindow("win_ser_3");
+        selenium.close();
+        selenium.selectWindow("win_ser_local");
+        selenium.click("link=2");
+        
+        System.out.println(LocalDateTime.now().toString());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+}
